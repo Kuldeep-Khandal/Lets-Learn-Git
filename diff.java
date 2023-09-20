b76517245940fdb6bc2ce9dd4b604a23fd03403c
Kuldeep Khandal <khandalkuldeep2004@gmail.com>
2023-09-20 22:44:17 +0530

    added message in sum.java

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diff {
     public static void main(String[] args) {
         int a = 7;
         int b = 6;
-        System.out.println(a-b);
+        System.out.println(a-b); 
     }
 }
