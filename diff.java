400dde1778c9f92561e9f0a7260d6976afc67280
Kuldeep Khandal <khandalkuldeep2004@gmail.com>
2023-09-21 17:08:00 +0530

    added implementation for diff.java

--- diff.java
+++ diff.java
@@ -1,7 +1,9 @@
 public class diff {
     public static void main(String[] args) {
-        int a = 7;
-        int b = 6;
-        System.out.println(a-b); 
+        int a = 7,c = 8;
+        int b = 6, d =5;
+        System.out.println(a-b);
+
+        System.out.println(c-d);
     }
 }
